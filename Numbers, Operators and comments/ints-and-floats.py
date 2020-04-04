6b84fdcdcd628b425ecc22212cbdfbd344dffc75
mehditaibi <mr.mehdi.taibi@gmail.com>
2020-04-03 19:26:56 -0700

    add numbers, operators and comments

---
+++ Numbers, Operators and comments/ints-and-floats.py
@@ -0,0 +1,8 @@
+type(9)
+# <class 'int'>
+
+type(9.0)
+# <class 'float'>
+
+1 + 1.0 
+# 2.0
---
+++ Numbers, Operators and comments/werider-operators.py
@@ -0,0 +1,11 @@
+# Exponention
+2 ** 3 
+# 8
+
+# Modulo
+10 % 3 
+# 1
+
+# Integer Division 
+10 // 3 
+# 3
